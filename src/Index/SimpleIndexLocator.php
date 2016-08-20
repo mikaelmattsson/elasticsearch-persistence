2a4915a22a1364b19e907c981da2efd34b4d3da0
Mikael Mattsson <mikael.p.mattsson@gmail.com>
2016-08-20 09:52:48 +0200

    Add xdebug

--- src/Index/SimpleIndexLocator.php
+++ src/Index/SimpleIndexLocator.php
@@ -7,6 +7,7 @@ class SimpleIndexLocator implements IndexLocatorInterface
     public function get(string $documentClassName) : IndexInterface
     {
         $indexClassName = $documentClassName.'Index';
-        return new $indexClassName;
+
+        return new $indexClassName();
     }
 }
